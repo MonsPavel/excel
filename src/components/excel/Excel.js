6738e6c908b7c2fd1b3a6ac03c5fbfdc3fb1b177
Mons Pavel <monspavel@gmail.com>
2020-10-10 13:23:06 +0300

    finish redux and table logic

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,18 +1,22 @@
 import { $ } from '@core/dom'
 import {Emitter} from '@core/Emitter'
+import {StoreSubscriber} from '@core/StoreSubscriber';
 
 export class Excel {
     constructor(selector, options) {
         this.$el = $(selector)
         this.componets = options.components || []
+        this.store = options.store
         this.emitter = new Emitter()
+        this.subscrier = new StoreSubscriber(this.store)
     }
 
     getRoot() {
         const $root = $.create('div', 'excel')
 
         const componentOptions = {
-            emitter: this.emitter
+            emitter: this.emitter,
+            store: this.store
         }
 
         this.componets = this.componets.map(Component => {
@@ -29,10 +33,12 @@ export class Excel {
     render() {
         this.$el.append(this.getRoot())
 
+        this.subscrier.subscribeComponents(this.componets)
         this.componets.forEach(component => component.init())
     }
 
     destroy() {
+        this.subscrier.usubscribeFromeStore()
         this.componets.forEach(component => component.destroy())
     }
 }
